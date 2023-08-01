d5e34751312c82a7526a705c2e93979efc2329fe
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-01 12:16:37 +0200

    Fix bug in trap hover

--- Trap is Mechanical or Explosive/Code/Script.lua
+++ Trap is Mechanical or Explosive/Code/Script.lua
@@ -1,12 +1,28 @@
+CombatActions.Interact_Disarm.GetActionDisplayName = function(self, units)
+  local trapName = T(726087963038, "Trap")
+  local isMechanical = false
 
-CombatActions.Interact_Disarm.GetActionDisplayName = function (self, units)
   for i, u in ipairs(units) do
-    if IsKindOf(u, "BoobyTrappable") then
-      if u.boobyTrapType == 3 or u.boobyTrapType == 4 then
-        return 'Disable Mechanical ' .. u.GetTrapDisplayName()
-      end
+    if u.GetTrapDisplayName then
+      trapName = u:GetTrapDisplayName()
     end
   end
 
-  return 'Defuse Explosive ' .. u.GetTrapDisplayName()
+  for i, u in ipairs(units) do
+    if IsKindOf(u, "BoobyTrappable") and (u.boobyTrapType or 0) >= 1 then
+      if u.boobyTrapType == 3 or u.boobyTrapType == 4 then
+        isMechanical = true
+      end
+      if u.GetTrapDisplayName then
+        trapName = u:GetTrapDisplayName()
+      end
+
+    end
+  end
+
+  return T({ 0, '<type_prefix> <trap_in><trap_name>',
+             type_prefix = isMechanical and 'Disable Mechanical' or 'Defuse Explosive',
+             trap_in = TGetID(trapName) ~= 726087963038 and 'Trap in ' or '',
+             trap_name = trapName
+  })
 end
