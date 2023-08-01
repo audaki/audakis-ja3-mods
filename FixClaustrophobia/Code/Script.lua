36ee767fcb0e9957e47b1963371c49bb36dc6d41
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-01 21:13:46 +0200

    Properly fix claustrophobia removal

--- FixClaustrophobia/Code/Script.lua
+++ FixClaustrophobia/Code/Script.lua
@@ -1,3 +1,46 @@
+function Unit:Despawn()
+  self:AutoRemoveCombatEffects()
+  self:InterruptPreparedAttack()
+  self:RemoveEnemyPindown()
+  local dlg = GetInGameInterfaceModeDlg()
+  local crosshair = dlg and dlg.crosshair
+  if crosshair and crosshair.context.target == self then
+    dlg:RemoveCrosshair("Despawn")
+    if g_Combat then
+      if g_Combat:ShouldEndCombat() then
+        g_Combat:EndCombatCheck(true)
+      else
+        SetInGameInterfaceMode("IModeCombatMovement")
+      end
+    else
+      SetInGameInterfaceMode("IModeExploration", {suppress_camera_init = true})
+    end
+  end
+  if not self:IsAmbientUnit() then
+    self:SyncWithSession("map")
+  end
+  self:RemoveAllStatusEffects('despawn')
+  if SelectedObj == self then
+    SelectObj()
+  end
+  if self.Squad then
+    local squad = gv_Squads[self.Squad]
+    if squad and (squad.Side == "enemy1" or squad.Side == "enemy2") then
+      RemoveUnitFromSquad(gv_UnitData[self.session_id])
+    end
+  elseif not IsMerc(self) then
+    gv_UnitData[self.session_id] = nil
+  end
+  if g_Combat and not self:IsLocalPlayerControlled() and CountAnyEnemies() == 1 then
+    g_Combat.retreat_enemies = true
+  end
+  DoneObject(self)
+  if g_Combat and not g_AIExecutionController then
+    g_Combat:EndCombatCheck("force")
+  end
+end
+
+
 function StatusEffectObject:RemoveStatusEffect(id, stacks, reason)
   local has = self:HasStatusEffect(id)
   if not has then
@@ -13,7 +56,7 @@ function StatusEffectObject:RemoveStatusEffect(id, stacks, reason)
       and self ~= gv_UnitData[self.session_id]
       and self.team
       and (self.team.side == "player1" or self.team.side == "player2")
-      and id ~= 'Wounded'
+      and reason ~= 'despawn'
 
   if not effect.stacks then
     table.remove(self.StatusEffects, has)
