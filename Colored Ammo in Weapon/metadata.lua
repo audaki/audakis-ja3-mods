5c7b36a27d88e83d1cf573810226a9c43cf29deb
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-03 01:13:39 +0200

    Add colored ammo mod

---
+++ Colored Ammo in Weapon/metadata.lua
@@ -0,0 +1,19 @@
+return PlaceObj('ModDef', {
+	'title', "Colored Ammo in Weapon",
+	'description', "Colorize Ammo in Weapons (same color they have when hovering the ammo in Squad Supplies)\n\nSafe to add and remove at any time\n\nEnjoy :-)",
+	'image', "Mod/XadGsqw/Images/colored-ammo-in-weapons.png",
+	'last_changes', "Initial Release",
+	'id', "XadGsqw",
+	'content_path', "Mod/XadGsqw/",
+	'author', "Audaki_ra",
+	'version_major', 1,
+	'version', 52,
+	'lua_revision', 233360,
+	'saved_with_revision', 338408,
+	'code', {
+		"Code/Script.lua",
+	},
+	'saved', 1691016628,
+	'code_hash', -3040379961441335322,
+	'steam_id', "3014029872",
+})
