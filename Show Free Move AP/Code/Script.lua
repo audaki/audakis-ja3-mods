744149e69c847a193cce9523d5f88a288e10ede6
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-02 13:32:59 +0200

    Add Show Free Move AP mod

---
+++ Show Free Move AP/Code/Script.lua
@@ -0,0 +1,115 @@
+
+function audaFindXtListByTextId(obj, id)
+  local matches = {}
+  if obj.Text and TGetID(obj.Text) == id then
+    return { obj }
+  end
+  for it1, sub in ipairs(obj) do
+    local sub_matches = audaFindXtListByTextId(sub, id)
+    for it2, sub_match in ipairs(sub_matches) do
+      table.insert(matches, sub_match)
+    end
+  end
+  return matches
+end
+
+function audaFindXtByComment(obj, comment)
+  if (obj.comment or '') == comment then
+    return obj
+  end
+  for _, sub in ipairs(obj) do
+    local match = audaFindXtByComment(sub, comment)
+    if match then
+      return match
+    end
+  end
+end
+
+XTemplates.PDAMercRollover[1].Margins = box(42, 1, 0, 0)
+
+local mercRolloverApXt = audaFindXtByComment(XTemplates.PDAMercRollover, 'ap indicator')
+if mercRolloverApXt then
+  mercRolloverApXt.OnContextUpdate = function(self, context, ...)
+    local text
+    if g_Combat then
+      local currentAP = context:GetUIActionPoints()
+      local maxAP = context:GetMaxActionPoints()
+      local bonus = context.free_move_ap
+      local ctx = SubContext(context, {
+        current = currentAP,
+        bonus = bonus,
+      })
+      if 1000 <= bonus then
+        text = T({
+          158709477285,
+          "<apn(current)><style PDASMLevelTxt>+<apn(bonus)></style>",
+          ctx
+        })
+      else
+        text = T({
+          263805086279,
+          "<apn(current)>",
+          ctx
+        })
+      end
+    else
+      local maxAP = context:GetMaxActionPoints()
+      text = T({
+        330002924751,
+        "<apn(maxActionPoints)>",
+        maxActionPoints = maxAP
+      })
+    end
+    text = text .. " " .. T(363250742550, "<style PDARolloverHeaderDark>AP</style>")
+    self:SetText(text)
+    XContextControl.OnContextUpdate(self, context)
+  end
+end
+
+local apDisplays = audaFindXtListByTextId(XTemplates.SquadsAndMercs, 219068997732)
+for _, xt in ipairs(apDisplays) do
+  xt.OnContextUpdate = function(self, context, ...)
+    local text
+    if not IsKindOf(context, "Unit") then
+      return
+    end
+    self.parent:SetVisible(not not g_Combat and not context:IsDead() and not context:IsDowned())
+
+    if g_Combat then
+      self.parent.MinWidth = 39
+      self.parent.MaxWidth = 49
+      local currentAP = context:GetUIActionPoints()
+      local maxAP = context:GetMaxActionPoints()
+      local bonus = context.free_move_ap
+      local ctx = SubContext(context, {
+        current = currentAP,
+        bonus = bonus,
+      })
+      if 1000 <= bonus then
+        text = T({
+          158709477285,
+          "<apn(current)><style PDASM_NewSquadLabel>+<apn(bonus)></style>",
+          ctx
+        })
+      else
+        text = T({
+          263805086279,
+          "<apn(current)>",
+          ctx
+        })
+      end
+    else
+      self.parent.MinWidth = 30
+      self.parent.MaxWidth = 30
+      local maxAP = context:GetMaxActionPoints()
+      text = T({
+        330002924751,
+        "<apn(maxActionPoints)>",
+        maxActionPoints = maxAP
+      })
+    end
+
+    self:SetText(text)
+    XContextControl.OnContextUpdate(self, context)
+  end
+end
---
+++ Show Free Move AP/metadata.lua
@@ -0,0 +1,19 @@
+return PlaceObj('ModDef', {
+	'title', "Show Free Move AP",
+	'description', "Internally Free Move actually has an AP number.\n\nThis mod shows the Free Move AP number.\n\nThat's it.\n\nSafe to add and remove at any time.\n\nEnjoy :-)",
+	'image', "Mod/YfDhN7f/Images/show-free-move-ap.png",
+	'last_changes', "Initial Release",
+	'id', "YfDhN7f",
+	'content_path', "Mod/YfDhN7f/",
+	'author', "Audaki_ra",
+	'version_major', 1,
+	'version', 70,
+	'lua_revision', 233360,
+	'saved_with_revision', 338408,
+	'code', {
+		"Code/Script.lua",
+	},
+	'saved', 1690975478,
+	'code_hash', 4719138490143976870,
+	'steam_id', "3013755062",
+})
