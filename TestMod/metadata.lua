89978f190c1e2af769250adf02544d90a757d44f
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-04 13:22:56 +0200

    inventory test mod

---
+++ TestMod/metadata.lua
@@ -0,0 +1,14 @@
+return PlaceObj('ModDef', {
+	'title', "TestMod",
+	'id', "iQrcf5a",
+	'content_path', "Mod/iQrcf5a/",
+	'author', "Audaki_ra",
+	'version', 293,
+	'lua_revision', 233360,
+	'saved_with_revision', 339125,
+	'code', {
+		"Code/Script.lua",
+	},
+	'saved', 1691148083,
+	'code_hash', 7871860093305533356,
+})
