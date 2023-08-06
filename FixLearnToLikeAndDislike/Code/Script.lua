e3ee2ba82af1c98fcde7a5ea02e42128f4509bc1
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-06 03:46:12 +0200

    Fix issue with table references

--- FixLearnToLikeAndDislike/Code/Script.lua
+++ FixLearnToLikeAndDislike/Code/Script.lua
@@ -2,12 +2,14 @@
 
 function OnMsg.BecomeLiked(likerId, likeeId)
   if g_Units and g_Units[likerId] then
+    g_Units[likerId].Likes = table.copy(g_Units[likerId].Likes)
     table.insert(g_Units[likerId].Likes, likeeId)
   end
 end
 
 function OnMsg.BecomeDisliked(dislikerId, dislikeeId)
   if g_Units and g_Units[dislikerId] then
+    g_Units[dislikerId].Dislikes = table.copy(g_Units[dislikerId].Dislikes)
     table.insert(g_Units[dislikerId].Dislikes, dislikeeId)
   end
 end
