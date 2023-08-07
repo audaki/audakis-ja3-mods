ac7bed3edf165031911147dd0aa810822827d3f9
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-07 03:47:33 +0200

    metadata

--- TestMod/metadata.lua
+++ TestMod/metadata.lua
@@ -3,13 +3,13 @@ return PlaceObj('ModDef', {
 	'id', "iQrcf5a",
 	'content_path', "Mod/iQrcf5a/",
 	'author', "Audaki_ra",
-	'version', 452,
+	'version', 464,
 	'lua_revision', 233360,
 	'saved_with_revision', 339125,
 	'code', {
 		"Code/Script.lua",
 	},
 	'has_options', true,
-	'saved', 1691205529,
-	'code_hash', -1758888399519655820,
+	'saved', 1691366327,
+	'code_hash', -6671561910814807364,
 })
