1ecd403025832ed897640ac47cfd0350c3f3100a
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-08 22:44:06 +0200

    Make vicki code even safer

--- Fix Vicki Perk/Code/Script.lua
+++ Fix Vicki Perk/Code/Script.lua
@@ -2,10 +2,9 @@
 function FirearmBase:GetNumModifiableComponents()
   local n = 0
   for i, slot in ipairs(self.ComponentSlots) do
-    local component = self.components[slot.SlotType]
     -- Non-modifiable slots broke the perk
     -- Slots with only 1 available component broke the perk
-    if component and slot.Modifiable and (#slot.AvailableComponents >= 2 or slot.CanBeEmpty) then
+    if slot.Modifiable and (#slot.AvailableComponents >= 2 or slot.CanBeEmpty) then
       n = n + 1
     end
   end
