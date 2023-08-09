12337b24fa909e66bbc919fd74261e2b9119c67b
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-09 20:03:25 +0200

    metadata

--- KISS Sniper Rifle Balance/metadata.lua
+++ KISS Sniper Rifle Balance/metadata.lua
@@ -1,6 +1,6 @@
 return PlaceObj('ModDef', {
 	'title', "KISS Sniper Rifle Balance",
-	'description', "KISS (= Keep It Simple, Stupid) Balancing for Sniper Rifles.\n\nThis mod fixes the balancing to Assault Rifles by making Sniper Rifles just as deadly as they are now, but only in the hands of good mercs!\n\nAnd the worse your merc is in MRK, the better an Assault Rifle will be in comparison to the Sniper Rifle.\n\nThis is simply done by doubling the MRK scaling for Sniper Rifles.\n\nThe Scaling will be less harsh the more you aim. So shooting a sniper rifled unaimed is really only something for the highest of highest MRK mercs.\n\n=> Mod is safe to add and remove at any time! <=\n\n# Sniper Rifles with this mod (4x aimed)\n- 60 MRK => 28% Base CtH\n- 70 MRK => 48% Base CtH\n- 80 MRK => 68% Base CtH\n- 90 MRK => 88% Base CtH\n- 92 MRK => 92% Base CtH\n- 100 MRK => 100% Base CtH\n\n# Status Quo for Sniper Rifles (without this mod)\n- 60 MRK => 60% Base CtH\n- 70 MRK => 70% Base CtH\n- 80 MRK => 80% Base CtH\n- 90 MRK => 90% Base CtH\n- 92 MRK => 92% Base CtH\n- 100 MRK => 100% Base CtH\n\n# Sniper Rifles with this mod (unaimed)\n- 60 MRK => 20% Base CtH\n- 70 MRK => 40% Base CtH\n- 80 MRK => 60% Base CtH\n- 90 MRK => 80% Base CtH\n- 92 MRK => 84% Base CtH\n- 100 MRK => 100% Base CtH",
+	'description', "KISS (= Keep It Simple, Stupid) Balancing for Sniper Rifles.\n\nThis mod fixes the balancing to Assault Rifles by making Sniper Rifles just as deadly as they are now, but only in the hands of good mercs!\n\nAnd the worse your merc is in Marksmanship (MRK), the better an Assault Rifle will be in comparison to the Sniper Rifle.\n\nThis is simply done by doubling the MRK scaling for Sniper Rifles.\n\nThe Scaling will be less harsh the more you aim. So shooting a sniper rifled unaimed is really only something for the highest of highest MRK mercs.\n\n=> Mod is safe to add and remove at any time! <=\n\n# Vanilla Sniper Rifles (without this mod)\n- 100 MRK => 100% Base CtH\n- 95 MRK => 95% Base CtH\n- 90 MRK => 90% Base CtH\n- 80 MRK => 80% Base CtH\n- 70 MRK => 70% Base CtH\n- 60 MRK => 60% Base CtH\n\n# Sniper Rifles with this mod (4x aimed, aiming bonus is added on top!)\n- 100 MRK => 100% Base CtH + Aim Bonus\n- 95 MRK => 95% Base CtH + Aim Bonus\n- 90 MRK => 88% Base CtH + Aim Bonus\n- 80 MRK => 68% Base CtH + Aim Bonus\n- 70 MRK => 48% Base CtH + Aim Bonus\n- 60 MRK => 28% Base CtH + Aim Bonus\n\n# Sniper Rifles with this mod (unaimed)\n- 100 MRK => 100% Base CtH\n- 95 MRK => 90% Base CtH\n- 90 MRK => 80% Base CtH\n- 80 MRK => 60% Base CtH\n- 70 MRK => 40% Base CtH\n- 60 MRK => 20% Base CtH",
 	'image', "Mod/rRfFoWJ/Images/KISS-sniper-rifle-balance.png",
 	'last_changes', "Fix Scaling",
 	'id', "rRfFoWJ",
@@ -8,13 +8,13 @@ return PlaceObj('ModDef', {
 	'author', "Audaki_ra",
 	'version_major', 1,
 	'version_minor', 2,
-	'version', 30,
+	'version', 34,
 	'lua_revision', 233360,
 	'saved_with_revision', 339125,
 	'code', {
 		"Code/Script.lua",
 	},
-	'saved', 1691557044,
+	'saved', 1691593623,
 	'code_hash', -1562871743034519355,
 	'steam_id', "3017322956",
 })
