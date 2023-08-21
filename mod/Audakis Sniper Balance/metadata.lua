d65c3ff2dc0f6335b8c15b23ee83cdbb314148e1
Kira M. Backes <kira.backes@nrwsoft.de>
2023-08-21 23:26:49 +0200

    Update Burst Fire Balance to v1.1.0

--- mod/Audakis Sniper Balance/metadata.lua
+++ mod/Audakis Sniper Balance/metadata.lua
@@ -1,5 +1,5 @@
 return PlaceObj('ModDef', {
-	'title', "Audaki's Sniper Balance",
+	'title', "Audaki’s Sniper Balance",
 	'description', "Simple Balancing for Sniper Rifles in the way of KISS (= Keep It Simple, Stupid)\n\nCompatible with JA3 Patch v1.1.0\n\nThis mod fixes the balancing to Assault Rifles by making Sniper Rifles just as deadly as they are now, but only in the hands of good mercs!\n\nAnd the worse your merc is in Marksmanship (MRK) and Dexterity (DEX), the better an Assault Rifle will be in comparison to the Sniper Rifle.\n\nThis is done by adding a penalty to Sniper Rifles that can be overcome by Marksmanship, Dexterity (half scale) and Aiming Levels.\n\nReal-Life Examples:\n\nA Merc with 100 MRK and 94 DEX can shoot even unaimed Sniper Rifles perfectly\n\nScope will be able to shoot just as well as Vanilla if she adds two Aim Levels, otherwise she will have a miniscule penalty.\n\nBuns will be able to shoot just as well as Vanilla if she adds three Aim Levels, otherwise she will have a small penalty.\n\nKalyna with her starting stats will need to aim three times and still have a small penalty. If Kalyna tries to shoot unaimed she will have a big penalty.",
 	'image', "Mod/rRfFoWJ/Images/audakis-sniper-balance.png",
 	'last_changes', "Update for v1.1.0",
@@ -7,13 +7,13 @@ return PlaceObj('ModDef', {
 	'author', "Audaki_ra",
 	'version_major', 2,
 	'version_minor', 1,
-	'version', 107,
+	'version', 110,
 	'lua_revision', 233360,
 	'saved_with_revision', 340147,
 	'code', {
 		"Code/Script.lua",
 	},
-	'saved', 1692643983,
+	'saved', 1692652498,
 	'code_hash', -2550087624975448869,
 	'steam_id', "3017322956",
 })
